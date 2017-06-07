6118b9e3acda65eda4699e8cf67327f3a6c0c7ba
Ben NG Hok Chun <hcngac@gmail.com>
2017-06-07 10:50:38 +0800

    Change name from myos to nvramos

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/nvramos.kernel isodir/boot/nvramos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "nvramos" {
+	multiboot /boot/nvramos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o nvramos.iso isodir
