ddf8ac3763786c5693be419f5b94a1131df2dd70
Ben NG Hok Chun <hcngac@gmail.com>
2017-07-18 13:49:54 +0800

    Fixed section address not correct problem

--- kernel/arch/i386/mman/KernelSection.c
+++ kernel/arch/i386/mman/KernelSection.c
@@ -20,25 +20,25 @@ extern uint8_t __end_heap;
 KernelSection *ksection_getKsections() {
     static KernelSection ksects[KSECTION_COUNT + 1];
 
-    ksects[0].section = KSECTION_SECTION_TEXT;
-    ksects[0].addr = (uint64_t) &__start_text;
-    ksects[0].len = (uint64_t) (&__end_text - &__start_text);
+    ksects[KSECTION_SECTION_TEXT].section = KSECTION_SECTION_TEXT;
+    ksects[KSECTION_SECTION_TEXT].addr = (uint64_t) &__start_text;
+    ksects[KSECTION_SECTION_TEXT].len = (uint64_t) (&__end_text - &__start_text);
 
-    ksects[1].section = KSECTION_SECTION_RODATA;
-    ksects[1].addr = (uint64_t) &__start_rodata;
-    ksects[1].len = (uint64_t) (&__end_rodata - &__start_rodata);
+    ksects[KSECTION_SECTION_RODATA].section = KSECTION_SECTION_RODATA;
+    ksects[KSECTION_SECTION_RODATA].addr = (uint64_t) &__start_rodata;
+    ksects[KSECTION_SECTION_RODATA].len = (uint64_t) (&__end_rodata - &__start_rodata);
 
-    ksects[2].section = KSECTION_SECTION_DATA;
-    ksects[2].addr = (uint64_t) &__start_data;
-    ksects[2].len = (uint64_t) (&__end_data - &__start_data);
+    ksects[KSECTION_SECTION_DATA].section = KSECTION_SECTION_DATA;
+    ksects[KSECTION_SECTION_DATA].addr = (uint64_t) &__start_data;
+    ksects[KSECTION_SECTION_DATA].len = (uint64_t) (&__end_data - &__start_data);
 
-    ksects[3].section = KSECTION_SECTION_TSS;
-    ksects[3].addr = (uint64_t) &__start_tss;
-    ksects[3].len = (uint64_t) (&__end_tss - &__start_tss);
+    ksects[KSECTION_SECTION_TSS].section = KSECTION_SECTION_TSS;
+    ksects[KSECTION_SECTION_TSS].addr = (uint64_t) &__start_tss;
+    ksects[KSECTION_SECTION_TSS].len = (uint64_t) (&__end_tss - &__start_tss);
 
-    ksects[4].section = KSECTION_SECTION_BSS;
-    ksects[4].addr = (uint64_t) &__start_bss;
-    ksects[4].len = (uint64_t) (&__end_bss - &__start_bss);
+    ksects[KSECTION_SECTION_BSS].section = KSECTION_SECTION_BSS;
+    ksects[KSECTION_SECTION_BSS].addr = (uint64_t) &__start_bss;
+    ksects[KSECTION_SECTION_BSS].len = (uint64_t) (&__end_bss - &__start_bss);
 
     return ksects;
 }
