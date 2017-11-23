4ad9eb481711c2b9a80113e5c64c3f3c8e446010
Ben NG Hok Chun <hcngac@connect.ust.hk>
2017-11-23 14:34:31 +0800

    sync

--- kernel/include/kernel/datalayer/meta.h
+++ kernel/include/kernel/datalayer/meta.h
@@ -2,35 +2,35 @@
 #define NVMOS_DL_META_FLAG_ISDIR  0x2
 #define NVMOS_DL_META_FLAG_FREE   0x4
 
-struct NVMOS_DL_fileMeta{
-  uint64_t refCount; //Fiel reference is the access point to file
-  uint64_t contentLength;
-  uint64_t firstBlock;
-  uint64_t secondBlockPointers;
-  uint64_t thirdBlockPointers;
+struct NVMOS_DL_fileMeta {
+	uint64_t	refCount;           //Fiel reference is the access point to file
+	uint64_t	contentLength;
+	uint64_t	firstBlock;
+	uint64_t	secondBlockPointers;
+	uint64_t	thirdBlockPointers;
 };
 
-struct NVMOS_DL_processMeta{
-  uint32_t processId;
-  uint32_t parentId;
-  uint64_t pageTableLocation;
-  uint64_t pageTableLength;
-  uint64_t contentLength;
-  uint64_t firstBlock;
-  uint64_t secondBlockPointers;
-  uint64_t thirdBlockPointers;
+struct NVMOS_DL_processMeta {
+	uint32_t	processId;
+	uint32_t	parentId;
+	uint64_t	pageTableLocation;
+	uint64_t	pageTableLength;
+	uint64_t	contentLength;
+	uint64_t	firstBlock;
+	uint64_t	secondBlockPointers;
+	uint64_t	thirdBlockPointers;
 };
 
 struct NVMSO_DL_freeMeta {
-  uint64_t nextFreeMeta;
+	uint64_t nextFreeMeta;
 };
 
-struct NVMOS_DL_meta{
-  uint64_t metaId;
-  union{
-    datalayer_fileMeta_t fileMeta;
-    datalayer_processMeta_t processMeta;
-    datalayer_freeMeta_t freeMeta;
-  } metaContent;
-  uint32_t flags;
+struct NVMOS_DL_meta {
+	uint64_t	metaId;
+	union {
+		datalayer_fileMeta_t	fileMeta;
+		datalayer_processMeta_t processMeta;
+		datalayer_freeMeta_t	freeMeta;
+	} metaContent;
+	uint32_t	flags;
 };
