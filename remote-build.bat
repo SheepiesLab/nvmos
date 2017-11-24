82f545d7ef80f62498c66bccf0c7f27313fd356f
Ben NG Hok Chun <hcngac@gmail.com>
2017-11-24 11:27:42 +0800

    remote-build

--- remote-build.bat
+++ remote-build.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 aws ec2 start-instances --instance-ids i-0d522ff99edb890ca
 aws ec2 describe-instances --instance-ids i-0d522ff99edb890ca
 
@@ -7,13 +6,3 @@ git commit -m "Sync"
 git push
 
 bash -c "ssh ubuntu@nvmosdev.benbenng.net 'export PATH=\"/opt/cross/bin:$PATH\" && cd ~/workspace/nvmos && git pull &&./clean.sh && ./build.sh'"
-=======
-aws ec2 start-instances --instance-ids i-0d522ff99edb890ca
-aws ec2 describe-instances --instance-ids i-0d522ff99edb890ca
-
-git add .
-git commit -m "Sync"
-git push
-
-bash -c "ssh ubuntu@nvmosdev.benbenng.net 'export PATH=\"/opt/cross/bin:$PATH\" && cd ~/workspace/nvmos &&./clean.sh && ./build.sh'"
->>>>>>> f613b3372c865fc424fe71e2708b88c623b459aa
