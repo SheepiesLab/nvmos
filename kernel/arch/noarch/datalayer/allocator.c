4c125270a435b896fe7e0be6a88301dc172c4de0
Ben NG Hok Chun <hcngac@gmail.com>
2017-11-24 12:37:32 +0800

    Sync

--- kernel/arch/noarch/datalayer/allocator.c
+++ kernel/arch/noarch/datalayer/allocator.c
@@ -68,10 +68,66 @@ int nvmos_dl_alloc_retrieveAllocator(
 	return 0;
 }
 
+nvmos_dl_freeBlockListNode_t *_findFittestSegment(
+	nvmos_dl_freeBlockListNode_t *head,
+	size_t blockCount)
+{
+	if (head == NULL)
+		return NULL;
+
+	if (head->length == blockCount)
+		return head;
+
+	if (head->length < blockCount)
+	{
+		if (head->rightChild == NULL)
+			return NULL;
+		return _findFittestSegment(head->rightChild, blockCount);
+	}
+
+	if (head->length > blockCount)
+	{
+		if (head->leftChild == NULL)
+			return head;
+		nvmos_dl_freeBlockListNode_t *candidate =
+			_findFittestSegment(head->leftChild, blockCount);
+		if (candidate == NULL)
+			return head;
+		if (candidate->length > head->length)
+			return head;
+		else
+			return candidate;
+	}
+
+	return NULL;
+}
+
+int _popSegment(nvmos_dl_freeBlockListNode_t **head,
+				nvmos_dl_freeBlockListNode_t *target)
+{
+	nvmos_dl_freeBlockListNode_t *leftChild, rightChild, parent;
+	leftChild = target->leftChild;
+	rightChild = target->rightChild;
+	parent = target->parent;
+}
+
 nvmos_pointer nvmos_dl_alloc_allocateBlocks(
 	nvmos_dl_allocator_t *allocator,
 	size_t blockCount)
 {
+	nvmos_dl_freeBlockListNode_t **head = &(allocator->head);
+	nvmos_dl_freeBlockListNode_t *selectedSegment =
+		_findFittestSegment(*head, blockCount);
+	if (selectedSegment == NULL)
+		return NULL;
+
+	if (selectedSegment->sameValueNext == NULL)
+	{
+		_popSegment(head, selectedSegment);
+	}
+
+	while (selectedSegment->sameValueNext != NULL)
+		selectedSegment = selectedSegment->sameValueNext;
 }
 
 int nvmos_dl_alloc_deallocateBlocks(
