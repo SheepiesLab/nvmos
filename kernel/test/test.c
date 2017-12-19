07ec7f4f12afd821f15cc6bba35dfb331d4d7907
Ben NG Hok Chun <hcngac@gmail.com>
2017-12-19 16:07:09 +0800

    Sync

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -14,7 +14,7 @@ int nvmos_test_runAllTests(
     nvmos_ptr_t availMem,
     size_t availMemLen)
 {
-    printf("Avail Memory Address: %p\n", availMem);
-    printf("Avail Memory Size: %p\n", availMemLen);
+    printf("Test Memory Address: %p\n", availMem);
+    printf("Test Memory Size: %p\n", availMemLen);
     RUN_HEAP0_TEST0(availMem, availMemLen);
 }
