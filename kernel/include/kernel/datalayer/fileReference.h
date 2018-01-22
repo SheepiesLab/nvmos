6663ddc34bb0b385fa026f1e256764308289f08a
Ben NG Hok Chun <hcngac@gmail.com>
2018-01-22 12:17:46 +0800

    Sync

--- kernel/include/kernel/datalayer/fileReference.h
+++
@@ -1,9 +0,0 @@
-#define NVMOS_DL_FILENAMESIZE 2048
-
-struct datalayer_fileReference_struct {
-	uint64_t	metaId;
-	uint64_t	timeCreated;
-	uint64_t	timeLastModified;
-	uint64_t	timeLastOpened;
-	uint8_t		name[NVMOS_DL_FILENAMESIZE];
-}
