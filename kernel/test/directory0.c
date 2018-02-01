85c7ac2fca12cb7961a551c2eec404d9074434e5
Ben NG Hok Chun <hcngac@gmail.com>
2018-02-01 15:20:22 +0800

    Sync

--- kernel/test/directory0.c
+++ kernel/test/directory0.c
@@ -33,14 +33,14 @@ nvmos_test_testDriver(directory0, test0)
 
     for (int i = 0; i < 8; ++i)
     {
-    meta_setDir(&(dirs[i]));
-    dirs[i].metaContent.fileMeta.refCount = 0;
-    dirs[i].metaContent.fileMeta.fileSize = 0;
-    dirs[i].metaContent.fileMeta.blkSize = 0;
-    dirs[i].metaContent.fileMeta._1stBlk = NULL;
-    dirs[i].metaContent.fileMeta._1stPtrBlk = NULL;
-    dirs[i].metaContent.fileMeta._2ndPtrBlk = NULL;
-    dirs[i].metaContent.fileMeta._3rdPtrBlk = NULL;
+        meta_setDir(&(dirs[i]));
+        dirs[i].metaContent.fileMeta.refCount = 0;
+        dirs[i].metaContent.fileMeta.fileSize = 0;
+        dirs[i].metaContent.fileMeta.blkSize = 0;
+        dirs[i].metaContent.fileMeta._1stBlk = NULL;
+        dirs[i].metaContent.fileMeta._1stPtrBlk = NULL;
+        dirs[i].metaContent.fileMeta._2ndPtrBlk = NULL;
+        dirs[i].metaContent.fileMeta._3rdPtrBlk = NULL;
     }
 
     dir_addFileRef(&(dirs[0].metaContent.fileMeta), "1", &(dirs[1]), &allocator);
@@ -100,6 +100,54 @@ nvmos_test_testDriver(directory0, test0)
     if (dir_fileNameUsed(&(dirs[4].metaContent.fileMeta), "8"))
     {
         nvmos_test_fail("directory0", 0, "Directory Name 8 detected", 0);
+    }
+
+    dir_fileRef_t *fref01 = dir_getFileRefById(
+        &(dirs[0].metaContent.fileMeta),
+        dir_searchFileRef(&(dirs[0].metaContent.fileMeta), "1"));
+    if (fref01 == NULL)
+    {
+        nvmos_test_fail("directory0", 0, "Directory Search 0->1 failed", 0);
+    }
+    else
+    {
+        meta_meta_t *meta01 = (meta_meta_t *)(fref01->metaPtr);
+        if (meta01 != dirs[1])
+        {
+            nvmos_test_fail("directory0", 0, "Directory Search 0->1 is not consistent", 0);
+        }
+    }
+
+    dir_fileRef_t *fref02 = dir_getFileRefById(
+        &(dirs[0].metaContent.fileMeta),
+        dir_searchFileRef(&(dirs[0].metaContent.fileMeta), "2"));
+    if (fref02 == NULL)
+    {
+        nvmos_test_fail("directory0", 0, "Directory Search 0->2 failed", 0);
+    }
+    else
+    {
+        meta_meta_t *meta02 = (meta_meta_t *)(fref02->metaPtr);
+        if (meta02 != dirs[2])
+        {
+            nvmos_test_fail("directory0", 0, "Directory Search 0->2 is not consistent", 0);
+        }
+    }
+
+    dir_fileRef_t *fref03 = dir_getFileRefById(
+        &(dirs[0].metaContent.fileMeta),
+        dir_searchFileRef(&(dirs[0].metaContent.fileMeta), "3"));
+    if (fref03 == NULL)
+    {
+        nvmos_test_fail("directory0", 0, "Directory Search 0->3 failed", 0);
+    }
+    else
+    {
+        meta_meta_t *meta03 = (meta_meta_t *)(fref03->metaPtr);
+        if (meta03 != dirs[3])
+        {
+            nvmos_test_fail("directory0", 0, "Directory Search 0->3 is not consistent", 0);
+        }
     }
 
     return 0;
