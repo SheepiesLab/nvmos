c00faeb389b93f0a0ff1d64e6a046b4ced71fe1a
Ben NG Hok Chun <hcngac@gmail.com>
2018-02-28 19:32:32 +0800

    Sync

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -119,6 +119,12 @@ void kernel_main(multiboot_info_t *mbt)
         printf("DL length:      0x%p\n",
                (uint64_t)dlSize);
     }
+    nvmos_ptr_t testAlloc[16];
+    for (int i = 0; i < 16; ++i){
+        testAlloc[i] = nvmos_dl_alloc_allocateBlocks(&allocator, 1);
+        printf("Test Alloc:     0x%p\n",
+               (uint64_t)testAlloc[i]);
+    }
 
     // Add a process to kroot
     meta_meta_t *kroot = datalayer_getKRoot(dlMeta);
