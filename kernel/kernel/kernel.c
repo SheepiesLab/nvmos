1b7a915fb061e79ebf1be0791f052ca9a9ce706c
Ben NG Hok Chun <hcngac@gmail.com>
2018-03-01 15:33:53 +0800

    Sync

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -252,9 +252,7 @@ void kernel_main(multiboot_info_t *mbt)
         printf("Error mapping file0 to proc0\n\n");
         goto endProc;
     }
-
-    printf("Test Mem 1: %p\n", *(uint32_t *)twoBlocks);
-    printf("Test Mem 2: %p\n", *(uint32_t *)(twoBlocks + 0x2000 - 4));
+    
     nvmos_pagingOn(proc0Meta->pageDir);
     printf("Test Mem 1: %p\n", *(uint32_t *)0x40000000);
     printf("Test Mem 2: %p\n", *(uint32_t *)0x40001ffc);
