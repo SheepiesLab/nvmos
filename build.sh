8857c2de3ae36d9d80716d1c3051aadc29a53ef6
Ben NG Hok Chun <hcngac@connect.ust.hk>
2018-03-22 19:07:42 +0800

    Sync

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 . ./headers.sh
 
 for PROJECT in $PROJECTS; do
-  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
+  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install -j $(lscpu | grep ^CPU\(s\): | awk '{print $2}'))
 done
 
 objcopy --only-keep-debug ./kernel/nvramos.kernel ./debug.sym
